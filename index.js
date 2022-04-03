7038842c12c966ad90461e2d5d6aefc01640a483
mhfuad <mhfuad2@gmail.com>
2022-04-03 12:44:42 +0600

    package json start script added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.port || 3000)
+app.listen(port,()=>{
+  console.log(`Project started on ${port}`)
+})
