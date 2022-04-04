12695a9d25dbb8a43a01ac067c151464338af614
mhfuad <mhfuad2@gmail.com>
2022-04-04 14:52:53 +0600

    user crud

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,42 @@
 const models = require('../models');
-var user = models.user;
+var User = models.user;
 
 exports.getAllUser = async (req, res) => {
-    const users = await user.findAll();
+    const users = await User.findAll();
     res.send(users);
 }
 
-exports.createUser = (req, res) => {
-    // const user = user.create({
-    //     username: "fuad",
-    //     email: "fuad@gmail.com",
-    //     password: "esscsm",
-    // })
-    res.send(req);    
+exports.createUser = async (req, res) => {
+    const user = await User.create(req.body)
+    console.log(user.id)
+    res.json(user)
+}
+
+exports.getUser = async (req, res) => {
+    const user = await User.findAll({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.json(user)
+}
+
+exports.updateUser = async (req, res) => {
+    const user = await User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    if(user){
+        res.send("update success");
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    User.destroy({
+        where: {
+            id : req.params.id
+        }
+    })
+    res.send("delete success")
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,10 @@ module.exports = app => {
     var router = require("express").Router();
 
     router.get('/', user.getAllUser);
+    router.post('/', user.createUser);
+    router.get('/:id', user.getUser);
+    router.delete('/:id', user.deleteUser);
+    router.put('/:id', user.updateUser);
 
     app.use('/api/user', router);
 }
