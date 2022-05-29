d7e6be199793782a66efbad602150fb17c603a45
mhfuad <mhfuad2@gmail.com>
2022-05-29 15:58:09 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
